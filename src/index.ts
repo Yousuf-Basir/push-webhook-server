c75dc079ecf5e9b2676845d0a245f3f54b86c177
Yousuf Basir <gourabxz@gmail.com>
2026-04-26 22:34:56 +0600

    add redis password env variable

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,7 @@ if (process.env.REDIS_ENV === 'cpanel') {
     redis: {
       host: process.env.REDIS_HOST || 'localhost',
       port: parseInt(process.env.REDIS_PORT || '6379'),
+      password: process.env.REDIS_PASSWORD || undefined,
     }
   };
 }
